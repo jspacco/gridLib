ae00a38f3199fca3bedc782d9a7cb9dc957d8f5a
Michelle Dudley <25540019+michdud@users.noreply.github.com>
2020-01-22 15:38:21 -0800

    Adhere to repo style

--- gridLib/src/gridLib/SimpleGrid.java
+++ gridLib/src/gridLib/SimpleGrid.java
@@ -34,51 +34,53 @@ import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.ToolTipManager;
 
-public class SimpleGrid extends JFrame 
+public class SimpleGrid extends JFrame
 {
-
+    
     private int numRows;
     private int numCols;
     private Color[][] cells;
     private BufferedImage[] images;
-    protected boolean symbolMode = false;
-    protected boolean error = false;
+    protected boolean symbolMode=false;
+    protected boolean error=false;
     protected Color errorColor;
     protected int errorRow;
     protected int errorCol;
     protected String errorMessage;
-    protected final int MARGIN_SIZE = 5;
-    protected final int DOUBLE_MARGIN_SIZE = MARGIN_SIZE * 2;
-    protected int squareSize = 25;
+    protected final int MARGIN_SIZE=5;
+    protected final int DOUBLE_MARGIN_SIZE=MARGIN_SIZE*2;
+    protected int squareSize=25;
     protected JPanel canvas;
-    // protected boolean visible=true;
+    //protected boolean visible=true;
 
     private static final long serialVersionUID = 1L;
-
+    
     public SimpleGrid(int size) {
         this(size, size, true);
     }
-
+    
     public SimpleGrid(int size, boolean visible) {
         this(size, size, visible);
     }
-
+    
     public SimpleGrid(int numRows, int numCols) {
         this(numRows, numCols, true);
     }
-
+    
     public void setLetterMode(boolean letterMode) {
         this.symbolMode = letterMode;
     }
-
+    
     private void drawGrid(Graphics2D g) {
-        int offset = MARGIN_SIZE;
-
+        int offset=MARGIN_SIZE;
+        
         // has someone tried to set a square that is out of bounds?
         if (error) {
             // TODO: add an arrow if the square is way out of bounds
             g.setColor(errorColor);
-            g.fillRect((errorCol + 1) * squareSize + offset, (errorRow + 1) * squareSize + offset, squareSize,
+            g.fillRect((errorCol + 1) * squareSize + offset,
+                    (errorRow + 1) * squareSize + offset,
+                    squareSize,
                     squareSize);
         }
 
@@ -101,14 +103,22 @@ public class SimpleGrid extends JFrame
                 }
                 g.setColor(Color.BLACK);
 
-                g.drawLine(offset + c * squareSize, offset + r * squareSize, offset + (c + 1) * squareSize,
-                        offset + r * squareSize);
-                g.drawLine(offset + c * squareSize, offset + r * squareSize, offset + c * squareSize,
-                        offset + (r + 1) * squareSize);
-                g.drawLine(offset + (c + 1) * squareSize, offset + r * squareSize, offset + (c + 1) * squareSize,
-                        offset + (r + 1) * squareSize);
-                g.drawLine(offset + c * squareSize, offset + (r + 1) * squareSize, offset + (c + 1) * squareSize,
-                        offset + (r + 1) * squareSize);
+                g.drawLine(offset+c*squareSize,
+                        offset+r*squareSize,
+                        offset+(c+1)*squareSize,
+                        offset+r*squareSize);
+                g.drawLine(offset+c*squareSize,
+                        offset+r*squareSize,
+                        offset+c*squareSize,
+                        offset+(r+1)*squareSize);
+                g.drawLine(offset+(c+1)*squareSize,
+                        offset+r*squareSize,
+                        offset+(c+1)*squareSize,
+                        offset+(r+1)*squareSize);
+                g.drawLine(offset+c*squareSize,
+                        offset+(r+1)*squareSize,
+                        offset+(c+1)*squareSize,
+                        offset+(r+1)*squareSize);
             }
         }
 
@@ -151,16 +161,16 @@ public class SimpleGrid extends JFrame
 
     public SimpleGrid(int numRows, int numCols, boolean visible) {
         super("SimpleGrid");
-        this.numRows = numRows;
-        this.numCols = numCols;
-        this.cells = new Color[numRows][numCols];
+        this.numRows=numRows;
+        this.numCols=numCols;
+        this.cells=new Color[numRows][numCols];
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                cells[i][j] = Color.WHITE;
+                cells[i][j]=Color.WHITE;
             }
         }
 
-        this.images = new BufferedImage[10];
+        this.images=new BufferedImage[10];
         for (int i = 1; i < 11; i++) {
             if (i < 10) {
                 loadImage("symbol-0" + i + ".png", i - 1);
@@ -174,19 +184,19 @@ public class SimpleGrid extends JFrame
             // This is useful for testing with JUnit
             return;
         }
-        final JFrame outerFrame = this;
+        final JFrame outerFrame=this;
         createMenus();
-        final int numRows2 = this.numRows;
-        final int numCols2 = this.numCols;
-
-        canvas = new JPanel() {
-            private JFrame frame = outerFrame;
+        final int numRows2=this.numRows;
+        final int numCols2=this.numCols;
+        
+        canvas=new JPanel() {
+            private JFrame frame=outerFrame;
             private static final long serialVersionUID = 1L;
 
             @Override
             public void paint(Graphics graphics) {
-                Graphics2D g = (Graphics2D) graphics;
-
+                Graphics2D g=(Graphics2D) graphics;
+                
                 drawGrid(g);
 
                 // frame.setPreferredSize(new Dimension(numRows*squareSize + MARGIN_SIZE,
@@ -195,22 +205,29 @@ public class SimpleGrid extends JFrame
                         (numRows2 + 2) * squareSize + 2 * MARGIN_SIZE));
                 frame.pack();
             }
-
+            
         };
 
         canvas.addMouseListener(new MouseAdapter() {
             private void tooltip(String msg, MouseEvent e) {
                 // https://stackoverflow.com/questions/7353021/how-to-show-a-tooltip-on-a-mouse-click
-                JComponent component = (JComponent) e.getSource();
+                JComponent component = (JComponent)e.getSource();
                 component.setToolTipText(msg);
-                MouseEvent phantom = new MouseEvent(component, MouseEvent.MOUSE_MOVED, System.currentTimeMillis(), 0,
-                        e.getX(), e.getY(), 0, false);
+                MouseEvent phantom = new MouseEvent(
+                        component,
+                        MouseEvent.MOUSE_MOVED,
+                        System.currentTimeMillis(),
+                        0,
+                        e.getX(),
+                        e.getY(),
+                        0,
+                        false);
                 ToolTipManager.sharedInstance().mouseMoved(phantom);
             }
 
             @Override
             public void mouseClicked(MouseEvent e) {
-                Point p = e.getPoint();
+                Point p=e.getPoint();
                 // figure out the grid square
                 int col = (p.x - MARGIN_SIZE) / squareSize - 1;
                 int row = (p.y - MARGIN_SIZE) / squareSize - 1;
@@ -219,45 +236,41 @@ public class SimpleGrid extends JFrame
                     tooltip(msg, e);
                     System.out.printf(msg);
                 } else if (error) {
-                    if ((errorRow == -1 || errorRow == getNumRows()) && errorCol == col) {
-                        System.out.printf(errorMessage + "\n");
-                    } else if ((errorCol == -1 || errorCol == getNumCols()) && errorRow == row) {
-                        System.out.printf(errorMessage + "\n");
+                    if ((errorRow==-1 || errorRow==getNumRows()) && errorCol==col) {
+                        System.out.printf(errorMessage+"\n");
+                    } else if ((errorCol==-1 || errorCol==getNumCols()) && errorRow==row) {
+                        System.out.printf(errorMessage+"\n");
                     }
-                    String msg = "<html><p>" + errorMessage.replaceAll("\n", "</p><p>") + "</p></html>";
+                    String msg = "<html><p>"+errorMessage.replaceAll("\n", "</p><p>") + "</p></html>";
                     tooltip(msg, e);
                 }
             }
 
         });
 
-        // this.setSize(numCols * squareSize + 2*MARGIN_SIZE, numRows * squareSize +
-        // 2*MARGIN_SIZE);
-        // this.setPreferredSize(new Dimension(numCols * squareSize + 2*MARGIN_SIZE,
-        // numRows * squareSize + 2*MARGIN_SIZE));
+        // this.setSize(numCols * squareSize + 2*MARGIN_SIZE, numRows * squareSize + 2*MARGIN_SIZE);
+        // this.setPreferredSize(new Dimension(numCols * squareSize + 2*MARGIN_SIZE, numRows * squareSize + 2*MARGIN_SIZE));
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // setSize((numCols + DOUBLE_MARGIN_SIZE) * squareSize, (numRows +
-        // DOUBLE_MARGIN_SIZE) * squareSize);
+        // setSize((numCols + DOUBLE_MARGIN_SIZE) * squareSize, (numRows + DOUBLE_MARGIN_SIZE) * squareSize);
 
         // setContentPane(canvas);
         this.getContentPane().add(canvas, BorderLayout.CENTER);
         this.setResizable(true);
-
+        
         this.pack();
-
+        
         this.setLocation(100, 100);
         this.setVisible(true);
         this.toFront();
-
+        
         addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
+            @Override public void windowClosing(WindowEvent e) {
                 // dispose();
             }
         });
     }
-
+    
     /**
      * FIXME this doesn't resize properly
      * 
@@ -265,8 +278,7 @@ public class SimpleGrid extends JFrame
      */
     public void setSquareSize(int squareSize) {
         this.squareSize = squareSize;
-        // this.setPreferredSize(new Dimension(numCols * squareSize + 2*MARGIN_SIZE,
-        // numRows * squareSize + 2*MARGIN_SIZE));
+        // this.setPreferredSize(new Dimension(numCols * squareSize + 2*MARGIN_SIZE, numRows * squareSize + 2*MARGIN_SIZE));
         // this.pack();
         repaint();
     }
@@ -286,7 +298,7 @@ public class SimpleGrid extends JFrame
 
     public void setColor(int row, int col, Color color) {
         boundsCheck(row, col, color);
-        cells[row][col] = color;
+        cells[row][col]=color;
         repaint();
     }
 
@@ -297,109 +309,110 @@ public class SimpleGrid extends JFrame
         errorColor = color;
         boundsCheck(row, col);
     }
-
-    protected void boundsCheck(int row, int col) {
+    protected void boundsCheck(int row, int col)
+    {
         if (row >= 0 && row < getNumRows() && col >= 0 && col < getNumCols()) {
             // bounds are OK!
             return;
         }
-        error = true;
-
+        error=true;
+        
         if (row < 0 && col < 0) {
             // top left
-            errorRow = -1;
-            errorCol = -1;
+            errorRow=-1;
+            errorCol=-1;
         } else if (row < 0 && col >= getNumCols()) {
             // top right
-            errorRow = -1;
-            errorCol = getNumCols();
+            errorRow=-1;
+            errorCol=getNumCols();
         } else if (row < 0) {
             // row out of bounds, col is OK
-            errorRow = -1;
-            errorCol = col;
+            errorRow=-1;
+            errorCol=col;
         } else if (row >= getNumRows() && col < 0) {
             // bottom left
-            errorRow = getNumRows();
-            errorCol = -1;
+            errorRow=getNumRows();
+            errorCol=-1;
         } else if (row >= getNumRows() && col >= getNumCols()) {
             // bottom right
-            errorRow = getNumRows();
-            errorCol = getNumCols();
+            errorRow=getNumRows();
+            errorCol=getNumCols();
         } else if (row >= getNumRows()) {
             // row out of bounds, col is OK
-            errorRow = getNumRows();
-            errorCol = col;
+            errorRow=getNumRows();
+            errorCol=col;
         } else if (col < 0) {
             // row is OK at this point
             // left column
-            errorRow = row;
-            errorCol = -1;
-        } else if (col >= getNumCols()) {
+            errorRow=row;
+            errorCol=-1;
+        } else if (col >= getNumCols()) { 
             // right column
-            errorRow = row;
-            errorCol = getNumCols();
-        }
+            errorRow=row;
+            errorCol=getNumCols();
+        } 
         // Repaint to draw the current grid with the error indicated
         repaint();
-        errorMessage = String.format("(%d, %d) is out of bounds!\n", row, col)
-                + String.format("Row must be between %d and %d (you had %d)\n", 0, getNumRows() - 1, row)
-                + String.format("Col must be between %d and %d (you had %d)", 0, getNumCols() - 1, col);
+        errorMessage=String.format("(%d, %d) is out of bounds!\n", row, col)+
+                String.format("Row must be between %d and %d (you had %d)\n", 0, getNumRows()-1, row)+
+                String.format("Col must be between %d and %d (you had %d)", 0, getNumCols()-1, col);
         throw new IndexOutOfBoundsException(errorMessage);
     }
-
     private static BufferedImage getScreenShot(Component component) {
-        BufferedImage image = new BufferedImage(component.getWidth(), component.getHeight(),
+        BufferedImage image = new BufferedImage(
+                component.getWidth(),
+                component.getHeight(),
                 BufferedImage.TYPE_INT_RGB);
         // call the Component's paint method, using
         // the Graphics object of the image.
-        component.paint(image.getGraphics());
+        component.paint( image.getGraphics() );
         return image;
     }
-
-    public BufferedImage getScreenShot() {
+    
+    public BufferedImage getScreenShot(){
         // This is a hack, but I can't always export the image correctly,
         // especially when running in batch mode.
-        int width = (numCols + 2) * squareSize + 2 * MARGIN_SIZE;
-        int height = (numRows + 2) * squareSize + 2 * MARGIN_SIZE;
-
+        int width = (numCols+2)*squareSize + 2*MARGIN_SIZE;
+        int height = (numRows+2)*squareSize + 2*MARGIN_SIZE;
+        
         // BGR because we don't have an alpha channel
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
-
+        
         Graphics2D g = image.createGraphics();
 
         // First, paint everything white
         g.setPaint(Color.WHITE);
         g.fillRect(0, 0, image.getWidth(), image.getHeight());
-
+        
         drawGrid(g);
-
+        
         return image;
     }
-
+    
     protected void createMenus() {
-        JMenuBar menuBar = new JMenuBar();
+        JMenuBar menuBar=new JMenuBar();
 
-        JMenu menu = new JMenu("File");
+        JMenu menu=new JMenu("File");
         menuBar.add(menu);
 
         // save option
-        JMenuItem save = new JMenuItem("Save");
+        JMenuItem save=new JMenuItem("Save");
         menu.add(save);
-        final SimpleGrid frame = this;
+        final SimpleGrid frame=this;
         save.addActionListener(new ActionListener() {
-
-            private File currentDir = null;
-
+            
+            private File currentDir=null;
+            
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Save");
-                // BufferedImage img = getScreenShot(frame.getContentPane());
+                //BufferedImage img = getScreenShot(frame.getContentPane());
                 BufferedImage img = frame.getScreenShot();
 
-                // Create a file chooser
+                //Create a file chooser
                 final JFileChooser fc = new JFileChooser(currentDir);
                 fc.setSelectedFile(new File("Untitled.png"));
-
+                
                 // https://stackoverflow.com/questions/17103171/making-a-jfilechooser-select-the-text-of-the-file-name-but-not-the-extension
                 final JTextField textField = getTexField(fc);
                 if (textField != null) {
@@ -417,21 +430,26 @@ public class SimpleGrid extends JFrame
                         }
                     });
                 }
-
-                // In response to a button click:
+                
+                //In response to a button click:
                 int returnVal = fc.showSaveDialog(frame);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     try {
                         // write the image as a PNG
-                        ImageIO.write(img, "png", fc.getSelectedFile());
+                        ImageIO.write(
+                                img,
+                                "png",
+                                fc.getSelectedFile());
                         currentDir = fc.getSelectedFile().getParentFile();
-                    } catch (Exception ex) {
-                        JOptionPane.showMessageDialog(frame, "Unable to save file to " + fc.getSelectedFile().getName(),
-                                "error", JOptionPane.ERROR_MESSAGE);
+                    } catch(Exception ex) {
+                        JOptionPane.showMessageDialog(frame,
+                                "Unable to save file to "+fc.getSelectedFile().getName(),
+                                "error",
+                                JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
-
+        
             private JTextField getTexField(Container container) {
                 for (int i = 0; i < container.getComponentCount(); i++) {
                     Component child = container.getComponent(i);
@@ -447,11 +465,11 @@ public class SimpleGrid extends JFrame
                 return null;
             }
         });
-
+        
         menu.addSeparator();
 
         // quit option
-        JMenuItem quit = new JMenuItem("Quit");
+        JMenuItem quit=new JMenuItem("Quit");
         menu.add(quit);
         quit.addActionListener(new ActionListener() {
             @Override
@@ -463,9 +481,7 @@ public class SimpleGrid extends JFrame
         frame.setJMenuBar(menuBar);
     }
 
-    /*
-     * (non-Javadoc)
-     * 
+    /* (non-Javadoc)
      * @see java.lang.Object#hashCode()
      */
     @Override
@@ -476,9 +492,7 @@ public class SimpleGrid extends JFrame
         return result;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
+    /* (non-Javadoc)
      * @see java.lang.Object#equals(java.lang.Object)
      */
     @Override
@@ -494,5 +508,6 @@ public class SimpleGrid extends JFrame
             return false;
         return true;
     }
-
+    
+    
 }
